9fe4ef43ee6e9159c6889eb0234ef780da850b12
Sureinity <johnghlendealdo@gmail.com>
2025-06-22 10:19:25 +0800

    chore(api): set up /api/train/ endpoint for development and testing

--- backend/app/test_api.py
+++ backend/app/test_api.py
@@ -1,6 +1,5 @@
+from models import Payload
 from fastapi import FastAPI
-from pydantic import BaseModel
-from typing import Optional, List
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
@@ -13,17 +12,6 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-class MouseEvent(BaseModel):
-    x: int
-    y: int
-    timestamp: int
-
-class MousePayload(BaseModel):
-    UUID: str
-    data: List[MouseEvent]
-
-@app.post("/track-mouse/")
-def track_mouse(payload: MousePayload):
-    print(payload.UUID)
-    print(payload)
-    return "No data received"
+@app.post("/api/train/{userId}")
+def user_data(payload: Payload):
+    return payload
