d1ebfbdf7e0494f6e37d69b1afc57996be9f57cd
Ideore <fernylmaningo@gmail.com>
2025-10-15 12:36:36 +0800

    style(extension/frontend): update Navbar UI for improved layout and styling

--- extension/frontend/src/components/Navbar.tsx
+++ extension/frontend/src/components/Navbar.tsx
@@ -1,4 +1,5 @@
-import { TbUserFilled } from "react-icons/tb";
+import { IoSettingsSharp } from "react-icons/io5";
+import { TbLogs } from "react-icons/tb";
 import logo from "../assets/MaxiDOM-logo.png";
 
 const Navbar = () => {
@@ -8,13 +9,24 @@ const Navbar = () => {
           <div>
             <img src={logo} alt="MaxiDOM Logo" className="h-4 w-26 z-1" />
           </div>
-          <div className="tooltip tooltip-left z-1" data-tip="My Profile">
-            <button className="btn btn-outline btn-sm border-white hover:bg-white p-1 rounded-xl group">
-              <TbUserFilled
-                size={20}
-                className="text-white group-hover:text-gray-600 transition-colors duration-200"
+
+          <div className="flex gap-2">
+            <div className="tooltip tooltip-bottom" data-tip="Settings">
+            <button className="btn btn-neutral btn-sm p-2">
+              <IoSettingsSharp
+                size={16}
               />
             </button>
+          </div>
+
+          <div className="tooltip tooltip-bottom" data-tip="Logs">
+            <button className="btn btn-neutral btn-sm p-2">
+              <TbLogs
+                size={16}
+              />
+            </button>
+          </div>
+
           </div>
         </div>
     </>
